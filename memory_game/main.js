1ec5d9e0a3dea35bfeba6bf057cdeaca26e01072
Yashira Baggett <Yashira@Yashiras-MacBook-Air.local>
2019-03-24 06:29:06 -0700

    created game logic

---
+++ memory_game/main.js
@@ -0,0 +1,109 @@
+console.log("Up and running!");
+
+var memory_cards = [
+{
+
+rank:"queen",
+suit: "hearts",
+cardImage:"images/queen-of-hearts.png"
+
+rank:"queen",
+suit: "diamonds",
+cardImage:"images/queen-of-diamonds.png"
+
+rank:"king",     
+suit: "hearts",
+cardImage:"images/king-of-hearts.png"
+
+rank:"king",
+suit: "diamonds",
+cardImage:"images/king-of-diamonds.png"
+}
+
+//console.log( "User flipped" + cardOne);
+//console.log( "Use flipped" + cardThree);
+
+
+//Name your variables cardOne, cardTwo, cardThree, and cardFour.
+//var cardOne = "queen";
+var cards = [];
+//create an empty array-- cardsInPlay[]. This will eventually
+//hold the cards that are in play.
+
+//create a variable to represent the first card that the user flipped.
+//var cardOne = ["queen1"];
+card.push('queen1');
+//Use console.log() to display the card that the user flipped
+console.log(queen1);
+//var cardTwo = ["king"];
+cardsInPlay.push('king2');
+console.log( 'king2');
+
+//alert('Hello, friends.');  and you would create a pop up from your browser 
+//that displays the string 'Hello, friends'.
+//var cardOne = cards[0];
+//create an empty array--cardsInPlay[]. This will eventually
+//hold the cards that are in play.
+//var carOne = cards[0];
+//create an empty array--cardsInPlay[]. This will eventually
+//hold the cards that are in play.
+//cardsInPlay.push(cardOne);
+//console.log("User flipped " + cardOne);
+//cardsInPlay.push(cardOne);
+//console.log("User flipped queen " + cardOne);
+
+//var cardTwo = cards[2];
+//create an empty array--cardsInPlay[]. This will eventually
+//hold the cards that are in play.
+//cardsInPlay.push(cardTwo);
+//console.log("User flipped " + cardTwo);
+
+var cardsInPlay = [];
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+
+
+function flipCard = function(){
+//On first line inside the flipCard function, use the getAttribute 
+//method to get the data-id attribute of the card that was just clicked
+//and store it in a variable cardId.
+ //this.getAttribute('attribute-we-want-to-get-goes-here');
+   var cardId = this.getAttribute ('data-id');
+	//console.log("User flipped+");
+
+
+function createBoard () {
+	for (var i = 0; i < cards.length; i++) 
+var cardElement= document.createElement('images')
+cardElement(add)
+setAttribute(cardElement)=//?
+ this.setAttribute('src',cards[cardId.cardImage;
+ if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+
+ }
+//document.querySelector('.circle').addEventListener('click', turnRed);
+cardElement.addEventListener('click', flipCard);
+
+//Right before the if statement where you are checking to see
+ //if two cards have been played, use the setAttribute method
+ //to update the src attribute to the image of the card that was just clicked
+
+}
+function createBoard () {
+
+
+}
+
+
+
+
+{
+
+
